da725baff6aa53537c32576b91bd0a37f5fc7792
dzidzoiev <artur.dzidzoiev@gmail.com>
2015-11-30 23:32:42 +0200

    multiple tabs

---
+++ src/main/java/ua/kpi/dzidzoiev/is/controller/MainController.java
@@ -0,0 +1,11 @@
+package ua.kpi.dzidzoiev.is.controller;
+
+import javafx.fxml.FXML;
+
+/**
+ * Created by dzidzoiev on 11/30/15.
+ */
+public class MainController {
+    @FXML
+    SymmetricTabController symmTabCtrl;
+}
--- src/main/java/ua/kpi/dzidzoiev/is/controller/SymmetricTabController.java
+++ src/main/java/ua/kpi/dzidzoiev/is/controller/SymmetricTabController.java
@@ -4,4 +4,11 @@ package ua.kpi.dzidzoiev.is.controller;
  * Created by dzidzoiev on 11/30/15.
  */
 public class SymmetricTabController {
+    private boolean autoSelectKey;
+    private String key;
+    private boolean autoCreateFile;
+    private String sourceFilePath;
+    private String destFilePath;
+    private String cipherMode;
+    private String cipherAlg;
 }
